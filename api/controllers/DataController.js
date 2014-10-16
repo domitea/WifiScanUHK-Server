043d75d7b3f12af46e699139d628d8816848298a
Dominik Matoulek <domitea@gmail.com>
2014-10-16 20:17:15 +0200

    add data controller

---
+++ api/controllers/DataController.js
@@ -0,0 +1,31 @@
+/**
+ * DataController
+ *
+ * @description :: Server-side logic for managing data
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `DataController.upload()`
+   */
+  upload: function (req, res) {
+    return res.json({
+      todo: 'upload() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `DataController.download()`
+   */
+  download: function (req, res) {
+    return res.json({
+      todo: 'download() is not implemented yet!'
+    });
+  }
+};
+
